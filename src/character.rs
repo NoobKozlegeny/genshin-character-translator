558123366ac23ce64a8731e9f146ec98c6df17e2
TPéter Tóth <toth00peter@gmail.com>
2023-02-28 11:33:48 +0100

    Added character struct

---
+++ src/character.rs
@@ -0,0 +1,13 @@
+pub struct Character {
+    hu_names: Vec<String>,
+    en_name: String
+}
+
+impl Character {
+    pub fn new<T: Into<String> + Clone>(hu_names: &[T], en_name: T, ) -> Self {
+        Self {
+            hu_names: hu_names.to_vec().into_iter().map(Into::into).collect(),
+            en_name: en_name.into()
+        }
+    }
+}
